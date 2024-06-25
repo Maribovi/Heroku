233773e7a167f3909c055235750a1dc3e4cfd9f0
Mari Bovi <mailerbowler@yandex.ru>
2024-06-25 15:48:11 +0300

    Initial commit

---
+++ src/test/java/Checkboxes.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Checkboxes {
+
+    @Test
+public void checkboxes () {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    }
+}
